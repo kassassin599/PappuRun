10d54a49efcdd249aa69fe85dabc38b4711114b0
kassassin99 <kassassin99@gmail.com>
2020-04-13 18:31:49 +0530

    Pause menu up, functionality remaining

--- Assets/DonkeyKongCorona/Scripts/Enemy/EnemyAI.cs
+++ Assets/DonkeyKongCorona/Scripts/Enemy/EnemyAI.cs
@@ -15,8 +15,12 @@ public class EnemyAI : MonoBehaviour
 
     public bool _maskOn = false;
 
+    [SerializeField]
+    LayerMask layerMask;
+
     private void Start()
     {
+        Physics2D.queriesStartInColliders = false;
         StartCoroutine(MoveRight());
     }
 
@@ -26,24 +30,45 @@ public class EnemyAI : MonoBehaviour
         {
             transform.Translate(Vector3.left * Time.deltaTime * enemySpeed);
 
-            RaycastHit2D frontInfo = Physics2D.Raycast(detectChair.position, Vector2.left, 0.01f);
-            if (frontInfo.collider == true)
+            RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right *-1, 1, layerMask);
+            if (hitInfo.collider != null)
             {
-                if (/*frontInfo.collider.CompareTag("Enemy") || */frontInfo.collider.CompareTag("Wall"))
+                Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+                if (moveRight == true)
                 {
-
-                    if (moveRight == true)
-                    {
-                        transform.eulerAngles = new Vector3(0, -180, 0);
-                        moveRight = false;
-                    }
-                    else
-                    {
-                        transform.eulerAngles = new Vector3(0, 0, 0);
-                        moveRight = true;
-                    }
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    moveRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    moveRight = true;
                 }
             }
+            else
+            {
+                Debug.DrawLine(transform.position, transform.position + transform.right * -1, Color.green);
+                
+            }
+
+            //RaycastHit2D frontInfo = Physics2D.Raycast(detectChair.position, Vector2.left, 0.01f);
+            //if (frontInfo.collider == true)
+            //{
+            //    if (/*frontInfo.collider.CompareTag("Enemy") || */frontInfo.collider.CompareTag("Wall"))
+            //    {
+
+            //        if (moveRight == true)
+            //        {
+            //            transform.eulerAngles = new Vector3(0, -180, 0);
+            //            moveRight = false;
+            //        }
+            //        else
+            //        {
+            //            transform.eulerAngles = new Vector3(0, 0, 0);
+            //            moveRight = true;
+            //        }
+            //    }
+            //}
 
             yield return null;
         }
